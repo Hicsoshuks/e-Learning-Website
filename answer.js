72114a73b39068d0576c3ce83f5e658701682661
Emily <hicsoshuks@gmail.com>
2015-09-17 09:32:17 +1000

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,39 @@
+function textColor() {
+    document.getElementById("answer").style.color = "#AD85FF";
+}
+
+function textColor02() {
+    document.getElementById("answer02").style.color = "#AD85FF";
+}
+
+function textColor03() {
+    document.getElementById("answer03").style.color = "#AD85FF";
+}
+
+function textColor04() {
+    document.getElementById("answer04").style.color = "#AD85FF";
+}
+
+function textColor05() {
+    document.getElementById("answer05").style.color = "#AD85FF";
+}
+
+function textColor06() {
+    document.getElementById("answer06").style.color = "#AD85FF";
+}
+
+function textColor07() {
+    document.getElementById("answer07").style.color = "#AD85FF";
+}
+
+function textColor08() {
+    document.getElementById("answer08").style.color = "#AD85FF";
+}
+
+function textColor09() {
+    document.getElementById("answer09").style.color = "#AD85FF";
+}
+
+function textColor10() {
+    document.getElementById("answer10").style.color = "#AD85FF";
+}
